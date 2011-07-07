6e64658d645c944d2090ec8b09027fc1c3cbf285
Greatman <lebelwilliam@gmail.com>
2011-07-06 22:48:22 -0400

    0.5 release Testing jenkins variables

--- src/main/java/com/lebelw/Tickets/commands/TemplateCmd.java
+++ src/main/java/com/lebelw/Tickets/commands/TemplateCmd.java
@@ -18,7 +18,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandException;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
